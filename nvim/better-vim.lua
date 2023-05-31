710ed29af0e4727e5e1fafd755c28ccbc897c526
Matheo <matheo67450@gmail.com>
2023-05-31 19:12:25 +0200

    Update better-vim.lua

--- nvim/better-vim.lua
+++ nvim/better-vim.lua
@@ -1,33 +1,77 @@
-local lsps = require "better-vim.lsps"
-local nvim_tree = require "better-vim.nvim-tree"
-local lualine = require "better-vim.lualine"
-local custom_mappings = require "better-vim.custom-mappings"
-local plugins = require "better-vim.plugins"
+local M = {}
 
-return {
-  theme = {
-    name = "catppuccin",
-    catppuccin_flavour = "mocha",
+M.noice = {
+  cmdline = {
+    format = {
+      search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+      search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+    },
   },
-  mappings = {
-    leader = ",",
-    custom = custom_mappings,
-  },
-  nvim_tree = nvim_tree,
-  lsps = lsps,
-  treesitter = {"c", "c_sharp", "css", "html", "javascript", "json", "lua", "python", "typescript", "cpp", "php", "sql"},
-  plugins = plugins,
-  lualine = lualine,
-  flags = {
-    disable_tabs = true,
-  },
-  hooks = {
-    after_setup = function()
-      -- Floatterm config
-      vim.g.floaterm_title = ""
-
-      -- ftdetect
-      vim.cmd [[ autocmd BufNewFile,BufRead *.mdx set filetype=markdown.jsx ]]
-    end,
+  messages = {
+    enabled = true,
+    view = "mini",
+    view_error = "mini",
+    view_warn = "mini",
+    view_history = "messages",
+    view_search = "virtualtext",
   },
 }
+
+M.plugins = {
+  "rescript-lang/vim-rescript",
+  "nkrkv/nvim-treesitter-rescript",
+  "devongovett/tree-sitter-highlight",
+  "Heliferepo/VimTek",
+  "github/copilot.vim"
+}
+
+M.mappings = {
+  leader = "\\",
+  custom = {
+    ["<leader>t"] = { "<cmd>ToggleTerm direction=float<cr>", "Toggle Terminal" },
+    ["<s-k>"] = { "5kzz", "Jump 5 lines above" },
+    ["<s-j>"] = { "5jzz", "Jump 5 lines below" },
+  }
+}
+
+M.theme = {
+  name = "catppuccin",
+  catppuccin_flavour = "mocha",
+}
+
+M.flags = {
+  disable_tabs = true
+}
+
+M.nvim_tree = {
+  view = {
+    adaptive_size = false
+  }
+}
+
+M.lualine = {
+  options = {
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+  }
+}
+
+M.hooks = {
+  after_setup = function()
+    vim.cmd("set relativenumber")
+  end
+}
+
+M.lsps = {
+    -- LSPs
+  clangd = {},
+  cssls = {},
+  pylsp = {},
+  tailwindcss = {},
+  html = {},
+  sqlls = {},
+}
+
+M.treesitter = {"c", "c_sharp", "css", "html", "javascript", "json", "lua", "python", "typescript", "cpp", "php", "sql"}
+
+return M
